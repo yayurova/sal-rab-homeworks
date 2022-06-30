663e7494aa4fdb7c0eeef982c2208ff6d8316606
yayurova <ya.yurova@mail.ru>
2022-06-30 16:52:57 +0300

    task_3_вщту

--- task_3/solution.js
+++ task_3/solution.js
@@ -19,7 +19,7 @@ function renderCartItem(item) {
     // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
        itemCountText = itemCountText + product.count;
     // 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` × `
-       itemCountText = itemCountText + '×';
+       itemCountText = itemCountText + ` × `;
     // 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
        itemCountText = itemCountText + product.price;
     // 5. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽ = `
